f1a2d2598d9a6202a16fa97eeee775b4cfe51150
pang <pang@zhaw.ch>
2019-03-14 10:48:19 +0100

    Completed syncer

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # etcdgitcore
 Core software components of the etcd managed git environment.
 
-## Goals:
+## Installation
+ - Install and configure etcd **(real etcd cluster not tested yet)**
+ - Install and configure git
+ - Create a github directory to store data files you create
+ - Install requirements:
+ ```
+ pip install -r requirements.txt
+ ```
+ - Setup the config file with:
+    - The path to your local Git Repository
+    - The name you want written in etcd as your operator name
+    - The path to import cloned data to
+    - The host and port of etcd
 
-- Create a research collaboration platform for microservices.
-Each collaborator can contribute their own tools and manage their own data in their own repos, but they should all be able to find each other's data and collaborate.
+## Usage
+
+Run the syncer program to either sync your data with the cluster or retrieve data from another operator in the cluster.
+
+### Sync Mode
+
+In sync mode the program will ask you which microservice hub your data is about, push it to your github remote and write an entry in etcd to help members of the cluster to find it.
+
+### Retrieve Mode
+
+In retrieve mode the program will ask you which hub you are interested in, then display available datasets by date updated, and ask to input a date, then show you which operators have data from that date. Finally, once you select an operator, it will clone their data to your import directory.
--- syncer.py
+++ syncer.py
@@ -1,18 +1,27 @@
 import etcd
 import git
 import datetime
-#python-etcd
-#GitPython
+import configparser
 
 
-client = etcd.Client(host='127.0.0.1', port=2379)
+config = configparser.ConfigParser()
+config.read('config.ini')
+workdir = config['WORKING_ENVIRONMENT']['WORKDIR']
+user = config['WORKING_ENVIRONMENT']['OPERATOR']
+importdir = config['WORKING_ENVIRONMENT']['IMPORTDIR']
+etcd_host = config['ETCD']['HOST']
+etcd_port = int(config['ETCD']['PORT'])
+
+
+client = etcd.Client(host=etcd_host, port=etcd_port)
 now = str(datetime.datetime.now().year) + "-" + str(datetime.datetime.now().month) + "-" + str(datetime.datetime.now().day)
 print(now)
 
-def sync(path):
-    #Push to github repo
-    repo = git.Repo(path)
 
+def sync(path, operator):
+    # Push to github repo
+    repo = git.Repo(path)
+    hub = str(input("Which microservice repository?"))
     print(repo.git.status())
     print(repo.git.add('.'))
     repo.index.commit("sync " + now)
@@ -20,27 +29,29 @@ def sync(path):
     print(repo.remotes.origin.url)
     repo.remotes.origin.push()
     print(repo.git.status())
+    # Write etcd entry
+    client.set(('/'+ hub + '/' + now + '/' + operator), repo.remotes.origin.url)
+    print(client.get('/'+ hub + '/' + now + '/' + operator).value)
 
-    #Write etcd entry
-    client.set(('/aws/' + now + '/panos'), repo.remotes.origin.url)
-    print(client.get('/aws/' + now + '/panos').value)
 
-
-def retrieve():
+def retrieve(path):
     dir = str(input("Which microservice repository?"))
     directory = client.get("/" + dir)
-
     # loop through directory children
     for result in directory.children:
         print(result.key)
-    #newdata('/home/panos/etcdgitdata')
     date = str(input("Which date?"))
     directory = client.get("/" + dir + "/" + date)
-
     # loop through directory children
     for result in directory.children:
         print(result.key)
     operator = str(input("Which operator?"))
     value = client.get("/" + dir + "/" + date + "/" + operator).value
-    print(value)
-retrieve()
+    git.Repo.clone_from(value, path)
+
+
+mode = str(input("Mode? (sync/retrieve)"))
+if mode == 'sync':
+    sync(workdir, user)
+elif mode == 'retrieve':
+    retrieve(importdir)
