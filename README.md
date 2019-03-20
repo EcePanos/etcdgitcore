51bba5dfdb5a12525c2f9c8c80db1a09951adb3e
pang <pang@zhaw.ch>
2019-03-20 11:34:31 +0100

    Cleanup and docs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# etcdgitcore
+# etcd-git-sync
 Core software components of the etcd managed git environment.
 
 ## Installation
@@ -19,10 +19,16 @@ Core software components of the etcd managed git environment.
 
 Run the syncer program to either sync your data with the cluster or retrieve data from another operator in the cluster.
 
-### Sync Mode
+#### Sync Mode
 
-In sync mode the program will ask you which microservice hub your data is about, push it to your github remote and write an entry in etcd to help members of the cluster to find it.
+In sync mode the program will ask you which microservice hub your data is about, push it to your github remote and write an entry in etcd to help members of the cluster to find it.  
+The commit message used during the github sync is generated as `sync <date>`
 
-### Retrieve Mode
+#### Retrieve Mode
 
 In retrieve mode the program will ask you which hub you are interested in, then display available datasets by date updated, and ask to input a date, then show you which operators have data from that date. Finally, once you select an operator, it will clone their data to your import directory.
+
+## Purpose
+
+The purpose of this tool is to facilitate collaboration in a global decentralized effort, where all collaborators sharing a single git repo would quickly become too cumbersome, and simply advertising their data on a website would have too much management overhead compared to an automated and distributed solution. Most importantly, the goal is for there to be no 'owner' or central governing body for the initiative that would cause a management bottleneck and impede research.    
+The tool's goal is to solve the problem with minimal overhead, and without forcing the users to share sensitive data about their resources (such as passwords and ssh keys) while at the same time being easy to integrate in a larger toolset/research workflow.
--- syncer.py
+++ syncer.py
@@ -11,47 +11,77 @@ user = config['WORKING_ENVIRONMENT']['OPERATOR']
 importdir = config['WORKING_ENVIRONMENT']['IMPORTDIR']
 etcd_host = config['ETCD']['HOST']
 etcd_port = int(config['ETCD']['PORT'])
-
-
 client = etcd.Client(host=etcd_host, port=etcd_port)
-now = str(datetime.datetime.now().year) + "-" + str(datetime.datetime.now().month) + "-" + str(datetime.datetime.now().day)
-print(now)
+today = str(datetime.datetime.today().year) + "-" + str(datetime.datetime.today().month) + "-" + str(datetime.datetime.today().day)
 
 
-def sync(path, operator):
+def sync():
     # Push to github repo
-    repo = git.Repo(path)
+    repo = git.Repo(workdir)
     hub = str(input("Which microservice repository?"))
-    print(repo.git.status())
-    print(repo.git.add('.'))
-    repo.index.commit("sync " + now)
-    print(repo.git.status())
-    print(repo.remotes.origin.url)
-    repo.remotes.origin.push()
-    print(repo.git.status())
+    try:
+        repo.git.add('.')
+        repo.index.commit("sync " + today)
+        print(repo.remotes.origin.url)
+        repo.remotes.origin.push()
+    except:
+        print("Error during git sync")
+        return
     # Write etcd entry
-    client.set(('/'+ hub + '/' + now + '/' + operator), repo.remotes.origin.url)
-    print(client.get('/'+ hub + '/' + now + '/' + operator).value)
+    try:
+        client.set(('/'+ hub + '/' + today + '/' + user), repo.remotes.origin.url)
+        print(client.get('/'+ hub + '/' + today + '/' + user).value)
+    except:
+        print("Error updating etcd")
+        return
 
 
-def retrieve(path):
+def retrieve():
     dir = str(input("Which microservice repository?"))
-    directory = client.get("/" + dir)
-    # loop through directory children
-    for result in directory.children:
-        print(result.key)
+    try:
+        directory = client.get("/" + dir)
+        # loop through directory children
+        for result in directory.children:
+            print(result.key)
+    except:
+        print("No such entry")
+        return
     date = str(input("Which date?"))
-    directory = client.get("/" + dir + "/" + date)
-    # loop through directory children
-    for result in directory.children:
-        print(result.key)
+    try:
+        directory = client.get("/" + dir + "/" + date)
+        # loop through directory children
+        for result in directory.children:
+            print(result.key)
+    except:
+        print("No such entry")
+        return
     operator = str(input("Which operator?"))
-    value = client.get("/" + dir + "/" + date + "/" + operator).value
-    git.Repo.clone_from(value, path)
+    try:
+        value = client.get("/" + dir + "/" + date + "/" + operator).value
+    except:
+        print("No such entry")
+        return
+    answer = str(input("Clone " + value + " to " + importdir + "?[y/n]"))
+    if answer == 'y':
+        try:
+            git.Repo.clone_from(value, importdir)
+        except:
+            print("Error cloning data")
+            return
 
-
-mode = str(input("Mode? (sync/retrieve)"))
-if mode == 'sync':
-    sync(workdir, user)
-elif mode == 'retrieve':
-    retrieve(importdir)
+if __name__ == '__main__':
+    print("Etcd-Git Sync:")
+    print("A simple tool to share and track git repositories in an etcd cluster")
+    print("Today it is " + today)
+    while True:
+        mode = str(input("Mode? (sync/retrieve/quit)"))
+        if mode == 'sync':
+            sync()
+        elif mode == 'retrieve':
+            retrieve()
+        elif mode == 'quit':
+            quit = str(input("Are you sure? [y/n]"))
+            if quit == 'y':
+                break
+        else:
+            print("Sorry, didn't catch that.")
